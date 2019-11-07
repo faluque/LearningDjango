9cf03d9b01294ad56937d58dbbb96bf00225fffd
Fernando Luque <luke_foca@hotmail.com>
2019-11-07 15:41:51 -0300

    Initial Django App

--- LearningDjango/LearningDjango/settings.py
+++ LearningDjango/LearningDjango/settings.py
@@ -78,9 +78,16 @@ WSGI_APPLICATION = 'LearningDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'thebardstale$valeriasa',
+        'USER': 'thebardstale',
+        'PASSWORD': '5RL*r9%ynfB#',
+        'HOST': 'thebardstale.mysql.pythonanywhere-services.com',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 
 
